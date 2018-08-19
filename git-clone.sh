085f62452b54bcc175ee628794334666fd6ac404
rt <rt@localhost.localdomain>
2018-08-19 10:09:36 -0400

    quick script to clone all git repos

---
+++ git-clone.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git clone git@github.com:rtyner/mediawiki-backup.git
+git clone git@github.com:rtyner/mac_convert.git
+git clone git@github.com:rtyner/cidr-calc.git
+git clone git@github.com:rtyner/dotfiles.git
+git clone git@github.com:rtyner/think-python.git
+git clone git@github.com:rtyner/python-garbage.git
+git clone git@github.com:rtyner/python-crash-course.git
+git clone git@github.com:rtyner/automate-the-boring-stuff.git
+git clone git@github.com:rtyner/rhcsa.git
+git clone git@github.com:rtyner/notes.git
+git clone git@github.com:rtyner/linux-command-ref.git
+git clone git@github.com:rtyner/sync-dotfiles.git
+git clone git@github.com:rtyner/private-config.git
+git clone git@github.com:rtyner/ssh-key-mult-host.git
+git clone git@github.com:rtyner/misc-scripts.git
+
