39f5dd15dbc8021667993ada3cfc73fb68e39e21
rt <rusty@rtyner.com>
2018-09-18 05:49:18 -0400

    changed formatting

--- elasticsearch-shard-health.sh
+++ elasticsearch-shard-health.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-#######################################################################################
-# Author: Rusty Tyner                                                                 #
-# Date: 2/18/18                                                                       #
-# Purpose: To retrieve shard health of elasticsearch cluster and report if red        #
-#######################################################################################
-
 # curl the REST API of your elasticsearch server
 curl http://ELASTIC_SEARCH_IP:9200/_cluster/health > ~/elastic_health.txt
 if cat ~/elastic_health.txt | grep red/|yellow >> /dev/null
